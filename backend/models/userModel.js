4d535683cd497f26d8cebf73ad583c19224022cf
Sourabh verma <150373765+sourabh1211@users.noreply.github.com>
2025-03-15 15:25:58 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
-
 mongoose.connect("mongodb+srv://Sourabh:sourabh112002@noteapp.4h5mp.mongodb.net/noteApp?retryWrites=true&w=majority&appName=NoteApp");
-
 const userSchema = new mongoose.Schema({
   username: String,
   name: String,
@@ -13,7 +11,5 @@ const userSchema = new mongoose.Schema({
     default: Date.now
   }
 });
-
 mongoose.model('User', userSchema);
-
 module.exports = mongoose.model('User');
