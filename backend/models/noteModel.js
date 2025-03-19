69c68835d00d5be334aecda94dfc6ebac94b4738
Sourabh verma <150373765+sourabh1211@users.noreply.github.com>
2025-03-19 23:25:21 +0530

    Update noteModel.js

--- backend/models/noteModel.js
+++ backend/models/noteModel.js
@@ -1,19 +1,15 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
 mongoose.connection.on("connected", () => {
     console.log("MongoDB connected successfully");
 });
-
 mongoose.connection.on("error", (err) => {
     console.error("MongoDB connection error:", err);
 });
-
 const notesSchema = new mongoose.Schema({
     title: String,
     description: String,
@@ -25,11 +21,8 @@ const notesSchema = new mongoose.Schema({
         default: Date.now
     }
 });
-
 const Notes = mongoose.model("Notes", notesSchema);
 module.exports = Notes;
-
-
 // const mongoose = require('mongoose');
 // mongoose.connect("mongodb://127.0.0.1:27017/noteApp");
 // const notesSchema = new mongoose.Schema({
