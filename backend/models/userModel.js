016534b98c76175a7c0ed3a6538dd9f3644d038d
Sourabh verma <150373765+sourabh1211@users.noreply.github.com>
2025-03-19 23:26:08 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,21 +1,16 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-
 const MONGO_URI = process.env.MONGO_URI || process.env.LOCAL_MONGO_URI;
-
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
 mongoose.connection.on("connected", () => {
     console.log("MongoDB connected successfully");
 });
-
 mongoose.connection.on("error", (err) => {
     console.error("MongoDB connection error:", err);
 });
-
 const userSchema = new mongoose.Schema({
     username: String,
     name: String,
@@ -27,16 +22,8 @@ const userSchema = new mongoose.Schema({
         default: Date.now
     }
 });
-
 const User = mongoose.model('User', userSchema);
 module.exports = User;
-
-
-
-
-
-
-
 // const mongoose = require('mongoose');
 // mongoose.connect("mongodb://127.0.0.1:27017/noteApp");
 // const userSchema = new mongoose.Schema({
