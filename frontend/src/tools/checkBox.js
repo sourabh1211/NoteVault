4ba6c824fa251da01c1c91af8abab0bcbc4658fc
Sourabh verma <150373765+sourabh1211@users.noreply.github.com>
2025-03-27 14:30:10 +0530

    Update checkBox.js

--- frontend/src/tools/checkBox.js
+++ frontend/src/tools/checkBox.js
@@ -1,18 +1,14 @@
 import React, { useEffect } from 'react'
-
 const CheckBox = ({ check, setCheck, title }) => {
-
   const toggleCheckBox = () => {
     let checkBox = document.querySelector('.checkBox');
     checkBox.classList.toggle("active");
     setCheck(!check);
   }
-
   useEffect(() => {
     let checkBox = document.querySelector('.checkBox');
-    check ? checkBox.classList.add("active") : checkBox.classList.remove("active"); //if check is true add active class to checkbox else remove it
+    check ? checkBox.classList.add("active") : checkBox.classList.remove("active");
   }, [check])
-
   return (
     <>
       <div onClick={toggleCheckBox} className="flex items-center gap-2 mb-3">
@@ -22,5 +18,4 @@ const CheckBox = ({ check, setCheck, title }) => {
     </>
   )
 }
-
 export default CheckBox
